50c0a6c626f64d744e50e3ec4b335e62dfc11ec7
panikdkernel <panikdkernel@gmail.com>
2020-01-29 11:56:09 +0530

    add scoreboard

--- pong.py
+++ pong.py
@@ -9,6 +9,10 @@ window.bgcolor("black")
 window.setup(width=800, height=600)
 window.tracer(0)
 
+# Scores
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -37,6 +41,15 @@ ball.penup()
 ball.goto(0, 0)
 ball.dx = 0.4
 ball.dy = 0.4
+
+# Scoreboard
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B: 0", align="center", font=("Courier", 24 , "normal"))
 
 # Game Functions
 def paddle_a_up():
@@ -86,10 +99,16 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24 , "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B: {}".format(score_a,score_b), align="center", font=("Courier", 24 , "normal"))
 
     # Paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 50 and ball.ycor() > paddle_b.ycor() - 50):
